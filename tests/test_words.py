8a8d864926755981a67b573b5acf91c959b614b3
Sushant Gautam <susant.gautam@gmail.com>
2024-03-19 15:59:34 +0100

    test count

--- tests/test_words.py
+++ tests/test_words.py
@@ -8,4 +8,12 @@ from words import find_first_word_that_starts_with
 
 def test_find_first_word_that_starts_with():
     words=["apple", "banana", "cherry", "date", "elderberry"]
-    assert find_first_word_that_starts_with(words, "a") == "apple"
+    assert find_first_word_that_starts_with(words, "e") == "elderberry"
+
+from count import count_words
+
+def test_count_words():
+    text = "apple banana cherry date elderberry"
+    assert count_words(text) == {"apple": 1, "banana": 1, "cherry": 1, "date": 1, "elderberry": 1}
+    text = "apple banana cherry date elderberry apple banana cherry date elderberry apple banana cherry date elderberry"
+    assert count_words(text) == {"apple": 3, "banana": 3, "cherry": 3, "date": 3, "elderberry": 3}
