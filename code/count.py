36e630eea96ef110e5024adea3ebe8e4e2180182
Sushant Gautam <susant.gautam@gmail.com>
2024-03-19 15:49:58 +0100

    add figure

--- code/count.py
+++ code/count.py
@@ -24,27 +24,41 @@ def count_words(text: str) -> Dict[str, int]:
     # grab the 10 most common words
     return dict(counts.most_common(10))
 
+import glob 
+from os.path import basename
+import matplotlib.pyplot as plt
 
 def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser(description=count_words.__doc__)
-    parser.add_argument("path", type=Path)
-    parser.add_argument("-o", "--output", type=Path, default=None)
+    parser.add_argument("--path", type=Path, required=True)
+    parser.add_argument("-o", "--output", type=Path, required=True)
     args = vars(parser.parse_args(argv))
+    all_files_in_dir =  glob.glob(str(args["path"]) + '/*.txt')
+    #import basename#
+    for file in all_files_in_dir:
+        file_obj = open(file, "r")
+        counts = count_words(file_obj.read())
+        output_file = args["output"] / (basename(file) + ".png")
+        # create if not path exist
+        output_file.parent.mkdir(parents=True, exist_ok=True)
+        # plot count to a bar chart
+        plt.bar(counts.keys(), counts.values())
+        plt.xlabel("Word")
+        plt.ylabel("Count")
+        plt.savefig(output_file)
+        plt.close()
 
-    # count the words
-    if not args["path"].is_file():
-        print(f"{args['path']} is not a file")
-        return 1
-    counts = count_words(args["path"].read_text())
-    output = json.dumps(counts, indent=4)
-    print(output)
 
-    # save the output
-    if args["output"] is not None:
-        args["output"].parent.mkdir(parents=True, exist_ok=True)
-        args["output"].write_text(output)
 
-    return 0
+    # output = json.dumps(counts_all, indent=4)
+    # print(output)
+
+    # # save the output
+    # if args["output"] is not None:
+    #     args["output"].parent.mkdir(parents=True, exist_ok=True)
+    #     args["output"].write_text(output)
+
+    # return 0
 
 
 if __name__ == "__main__":
